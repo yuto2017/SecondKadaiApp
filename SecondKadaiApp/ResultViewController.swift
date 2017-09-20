222e11c3604e17b49def89e740ac58871f7eabd2
moriyuto <yuto.mori01@g.softbank.co.jp>
2017-09-20 19:02:13 +0900

    課題：あいさつアプリ（SecondKadaiApp）を提出します。

---
+++ SecondKadaiApp/ResultViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ResultViewController.swift
+//  SecondKadaiApp
+//
+//  Created by yonghu on 2017/09/20.
+//  Copyright © 2017年 yuto.mori. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    @IBOutlet weak var label_show_name: UILabel!
+    
+    // 受け取るためのプロパティ（変数）を宣言しておく
+    var text_name:String = ""
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        // 上記では、x, y を 0 と宣言していたが、
+        // 1画面目のViewControllerから遷移するときにprepareForSegueで
+        // text_nameの値を新たに代入される
+        let result = text_name
+        label_show_name.text = "入力した名前は \(result) です"
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var input_name: UITextField!
+    
+    @IBAction func showname(_ sender: Any) {
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +24,17 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // segueから遷移先のResultViewControllerを取得する
+        let resultViewController:ResultViewController = segue.destination as! ResultViewController
+        // 遷移先のResultViewControllerで宣言しているtext_nameに値を代入して渡す
+        resultViewController.text_name =  input_name.text!
 
+    }
+    
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
+    }
 
 }
 
